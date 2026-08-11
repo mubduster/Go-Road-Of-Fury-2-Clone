959f466ec7236dac7a88976960025dcc86e7bc80
mubduster <mubassirmuneeb51@gmail.com>
2026-08-12 04:33:51 +0500

    added 3rd background, sky ,and broken cloud script causing crashes.

--- Go/main.go
+++ Go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand/v2"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -24,6 +25,11 @@ type Ground struct {
 	Pos     rl.Vector2
 	Texture rl.Texture2D
 }
+type Cloud struct {
+	Pos rl.Vector2
+	Texture rl.Texture2D
+	Scale float32
+}
 type CactusTextures struct {
 	Texture rl.Texture2D
 }
@@ -37,6 +43,8 @@ var Timer float32 = 0.0
 var Offset float32 = 0
 var BG1Offset float32 = 0
 var BG2Offset float32 = 0
+var BG3Offset float32 = 0
+var SkyOffset float32 = 0
 var dT float32 = 0.0
 
 func main() {
@@ -65,6 +73,9 @@ func main() {
 	RoadTexture := rl.LoadTexture("./Textures/Road.png")
 	BackGround1 := rl.LoadTexture("./Textures/Background1.png")
 	BackGround2 := rl.LoadTexture("./Textures/Background2.png")
+	BackGround3 := rl.LoadTexture("./Textures/Background3.png")
+	SkyTexture := rl.LoadTexture("./Textures/Sky.png")  
+	CloudTexture := rl.LoadTexture("./Textures/Cloud.png") //542, 163
 
 	Roads := []Ground{
 		{Pos: rl.NewVector2(0, World.Y-1900), Texture: RoadTexture},
@@ -80,10 +91,25 @@ func main() {
 		{Pos: rl.NewVector2(1920*15, World.Y-2700), Texture: BackGround1},
 	}
 	Background2s := []Ground{
-		{Pos:rl.NewVector2(0, 2000), Texture: BackGround2},
-		{Pos:rl.NewVector2(1920*5, 2000), Texture: BackGround2},
-		{Pos:rl.NewVector2(1920*10, 2000), Texture: BackGround2},
-		{Pos:rl.NewVector2(1920*15, 2000), Texture: BackGround2},
+		{Pos: rl.NewVector2(0, 2300), Texture: BackGround2},
+		{Pos: rl.NewVector2(1920*5, 2300), Texture: BackGround2},
+		{Pos: rl.NewVector2(1920*10, 2300), Texture: BackGround2},
+		{Pos: rl.NewVector2(1920*15, 2300), Texture: BackGround2},
+	}
+	Background3s := []Ground{
+		{Pos: rl.NewVector2(0, 500), Texture: BackGround3},
+		{Pos: rl.NewVector2(1920*2, 500), Texture: BackGround3},
+		{Pos: rl.NewVector2(1920*10, 500), Texture: BackGround3},
+		{Pos: rl.NewVector2(1920*15, 500), Texture: BackGround3},
+	}
+	Skys := []Ground{
+		{Pos: rl.NewVector2(0,-200), Texture: SkyTexture},
+		{Pos: rl.NewVector2(1920*5,-200), Texture: SkyTexture},
+		{Pos: rl.NewVector2(1920*10,-200), Texture: SkyTexture},
+		{Pos: rl.NewVector2(1920*15,-200), Texture: SkyTexture},
+	}
+	Clouds := []Cloud {
+		{Pos: rl.NewVector2(World.X - 3000, 0), Texture: CloudTexture},
 	}
 
 	for !rl.WindowShouldClose() {
@@ -99,6 +125,13 @@ func main() {
 		Offset -= float32(RoadSpeed)
 		BG1Offset -= float32(RoadSpeed) / 1.3
 		BG2Offset -= float32(RoadSpeed) / 2.5
+		BG3Offset -= float32(RoadSpeed) / 5.5
+		SkyOffset -= float32(RoadSpeed) / 15
+
+		Roll := rand.IntN(100)
+		if Roll > 90 {
+			Clouds = append(Clouds, Cloud{Pos: rl.NewVector2(World.X, rand.Float32()*200), Texture: CloudTexture, Scale: 1+rand.Float32()*4})
+		}
 
 		if Offset <= -(1920 * 4 * 5) {
 			Offset = 0
@@ -109,6 +142,20 @@ func main() {
 		if BG2Offset <= -(1920 * 4 * 5) {
 			BG2Offset = 0
 		}
+		if BG3Offset <= -(1920*4*5) {
+			BG3Offset = 0
+		}
+		if SkyOffset <= -(1920*4*5) {
+			SkyOffset = 0
+		}
+		
+		for i,C := range Clouds {
+			if C.Pos.X < -542 {
+				Clouds[i] = Clouds[len(Clouds)-1]
+				Clouds = Clouds[:len(Clouds)-1]
+			}
+			Clouds[i].Pos.X = float32(i*542*int(C.Scale)) + SkyOffset+ 10 
+		}
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Blue)
@@ -118,13 +165,18 @@ func main() {
 		rl.DrawRectangleV(rl.NewVector2(0, 0), rl.Vector2(World), rl.SkyBlue)
 		rl.DrawRectangleV(Car1.Pos, Car1.Size, rl.Red)
 
+		DrawGround(Skys, SkyOffset)
+
+		
+		DrawGround(Background3s, BG3Offset)
+		DrawClouds(Clouds)
+		rl.DrawTextureEx(Clouds[0].Texture, Clouds[0].Pos, 0.0, 5, rl.White)
+
 		DrawGround(Background2s, BG2Offset)
 
 		DrawGround(Background1s, BG1Offset) // Render Background 1
 
 		DrawGround(Roads, Offset) // Render Road
-
-		// rl.DrawTextureEx(CactusTextures[0].Texture, rl.NewVector2(1000, 3500), 0.0, 3.4, rl.White)
 
 		rl.EndMode2D()
 
@@ -149,3 +201,11 @@ func DrawGround(Ground []Ground, Offset float32) {
 	}
 
 }
+
+func DrawClouds(Clouds []Cloud) {
+	for _,C := range Clouds {
+		rl.DrawTextureEx(C.Texture, C.Pos, 0.0, 1+rand.Float32()*4, rl.White)
+	}
+}
+
+
